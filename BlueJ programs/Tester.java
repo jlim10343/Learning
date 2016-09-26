d16500a4c4a8616f93cc08384dfa54dc48dcebd8
jlim10343 <aiwen.joy.lim@gmail.com>
2016-09-26 18:09:23 -0400

    Add files via upload

---
+++ BlueJ programs/Tester.java
@@ -0,0 +1,8 @@
+public class Tester //We could put any name here
+{
+        public static void main(String args[])
+        {
+            System.out.print("Hello world");
+            System.out.println("Hello world again");
+        }
+    }
---
+++ Testwo.java
@@ -0,0 +1,9 @@
+public class Testwo
+{
+    public static void main(String args[])
+    {
+        String mm = "Hello";
+        String nx = "good buddy";
+        System.out.println(mm + " " + nx);
+    }
+}
